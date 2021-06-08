0074ec171252d0734eeacaf40db2e41619318691
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-06-08 07:56:09 -0500

    this is a fcommit

--- Ex3_03_06_2021.c
+++ Ex3_03_06_2021.c
@@ -1,13 +1,23 @@
+/********************************************************
+ * Ex03 -- program to print out the result of 10 different operations. *
+ * Not an especially earth-shattering program. *
+ * *
+ * Author: Israel Prado. *
+ * *
+ * Purpose: run the program and I will get the result. *
+ * *
+ * Usage: *
+ * run the program and watch out. *
+ ********************************************************/
+
 #include <stdlib.h>
 #include <stdio.h> // input output data
 #include <math.h> 
 #include <string.h> // string handling operations
-#define cm  60
-#define ch  3600
 
 int main(void) {
-float res;
-res = (4/1)-(4/3)+(4/5)-(4/7)+(4/9)-(4/11)+(4/13)-(4/15)+(4/17)-(4/19);
-printf("el resultado de la operacion es %f \n", res);
+float res; //I commit the result in a variable type float
+res = (4/1)-(4/3)+(4/5)-(4/7)+(4/9)-(4/11)+(4/13)-(4/15)+(4/17)-(4/19); //I do the operation
+printf("el resultado de la operacion es %f \n", res); //It prints the result
 
 }
