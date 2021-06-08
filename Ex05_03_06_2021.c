65490d4f4274db7be6d9f456ed16d7429a2eb3bd
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-06-08 07:50:35 -0500

    this is a fcommit

--- Ex05_03_06_2021.c
+++ Ex05_03_06_2021.c
@@ -1,3 +1,15 @@
+/********************************************************
+ * Ex05 -- program to print out a asterisco square. *
+ * Not an especially earth-shattering program. *
+ * *
+ * Author: Israel Prado. *
+ * *
+ * Purpose: Use of the propietis of printf. *
+ * *
+ * Usage: *
+ * only run the program and it will apears. *
+ ********************************************************/
+
 #include <stdlib.h>
 #include <stdio.h> // input output data
 #include <math.h> 
@@ -5,11 +17,11 @@
 
 
 int main(void) {
+printf("****\n"); //only is put a scuare whit the correspondent separation
 printf("****\n");
 printf("****\n");
 printf("****\n");
-printf("****\n");
-printf("\n");
+printf("\n"); //I put a line in white
 printf("****\n");
 printf("*  *\n");
 printf("*  *\n");
