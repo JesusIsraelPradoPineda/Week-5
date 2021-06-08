1fca9835fd9b2666b04a3ae7b3ebe3e3cfdf16c4
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-06-08 07:53:55 -0500

    this is a f commit

--- Ex2_03_06_2021.c
+++ Ex2_03_06_2021.c
@@ -1,3 +1,15 @@
+/********************************************************
+ * Ex02 -- program to print out the convertion from seconds to hours and seconds. *
+ * Not an especially earth-shattering program. *
+ * *
+ * Author: Israel Prado. *
+ * *
+ * Purpose: know the conversion into minutes and hours. *
+ * *
+ * Usage: *
+ * put the numbers asked and it will apears. *
+ ********************************************************/
+
 #include <stdlib.h>
 #include <stdio.h> // input output data
 #include <math.h> 
@@ -6,16 +18,16 @@
 #define ch  3600
 
 int main(void) {
-  float seg;
-  float min;
+  float seg; //I defined a float variable for the convertion
+  float min; //min and hour will be variables that will commit the result
   float hour;
-  printf("que cantidad vamos a converitr \n");
+  printf("que cantidad vamos a converitr \n"); //I ask a number
   scanf("%f",&seg);
-  min = seg / cm;
+  min = seg / cm; //I aplly the formula and then i print in the screen
   printf("el resultado es %f", min);
   printf(" minutos");
   printf("\n");
-  hour = seg / ch;
+  hour = seg / ch; //I use the convertion for hours
   printf("el resultado es %f", hour);
   printf(" horas");
   printf("\n");
