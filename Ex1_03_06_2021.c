0af4a632772130c5f1a78ca0f93fb543685c5024
JesusIsraelPradoPineda <83649450+JesusIsraelPradoPineda@users.noreply.github.com>
2021-06-08 07:48:06 -0500

    this is a f commit

--- Ex1_03_06_2021.c
+++ Ex1_03_06_2021.c
@@ -1,22 +1,34 @@
+/********************************************************
+ * Ex01 -- program to print out the area and volume of a cilinder. *
+ * Not an especially earth-shattering program. *
+ * *
+ * Author: Israel Prado. *
+ * *
+ * Purpose: to know the volme of a cilinder as well as the area. *
+ * *
+ * Usage: *
+ * put the numbers and it will apears. *
+ ********************************************************/
+
 #include <stdlib.h>
 #include <stdio.h> // input output data
 #include <math.h> 
 #include <string.h> // string handling operations
-#define pi 3.1416
+#define pi 3.1416 //variable type global and it has a especific value that will never change
 
 int main(void){
-  float radio;
-  float altura;
-  float resA;
+  float radio; //I define the things that i will work as radio and altura
+  float altura; 
+  float resA; //I defined here the variables of the results
   float resB;
   
-  printf ("dame radio hijo :v\n");
+  printf ("dame radio hijo :v\n"); //I ask the rate and commit it
   scanf("%f",&radio);
 
-  printf ("dame altura babu :v\n");
+  printf ("dame altura babu :v\n"); //I ask the heigth and i commit that
   scanf("%f", &altura);
 
-  printf ("tu altura fue ");
+  printf ("tu altura fue "); //I print the result of area and volume
   printf ("%f",altura);
   printf (" y tu radio de ");
   printf ("%f",radio);
